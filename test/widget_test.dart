d85c5cd606646089ac578b300b9c8bbc6adc0d27
vpuhoff <vpuhoff@live.ru>
2025-02-09 07:05:22 +0400

    Фикс теста

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:dungeon_game/main.dart';
 import 'package:dungeon_game/screens/game_screen.dart';
+
 
 void main() {
   testWidgets('Game screen initial render test', (WidgetTester tester) async {
