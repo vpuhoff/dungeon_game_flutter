498b7335cc7530b07756314fdae1405f7fc40e25
vpuhoff <vpuhoff@live.ru>
2025-02-09 07:12:59 +0400

    Форматирование

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:dungeon_game/screens/game_screen.dart';
 
-
 void main() {
   testWidgets('Game screen initial render test', (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: GameScreen()));
